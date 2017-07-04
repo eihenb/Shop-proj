89ecb3d5cd6458cbcec2fdb68b39fc95939b2f0c
Eddie <eihenb3333@gmail.com>
2017-07-04 10:26:47 +0100

    gulp improved

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,4 @@
 const gulp = require('gulp'),
-      sass = require('gulp-sass'),
       sourcemaps = require('gulp-sourcemaps'),
       autoprefixer = require('gulp-autoprefixer'),
       debug = require('gulp-debug'),
@@ -8,10 +7,15 @@ const gulp = require('gulp'),
       clean = require('gulp-clean'),
       rigger = require('gulp-rigger'),
       changed = require('gulp-changed'),
-      imagemin = require('gulp-imagemin');
+      imagemin = require('gulp-imagemin'),
+      
+      postcss = require('gulp-postcss'),
+      postCssNested = require('postcss-nested'),
+      postCssVars = require('postcss-simple-vars'),
+      postCssImport = require('postcss-import');
 
 
-var input = 'src/sass/**/*.sass';
+var input = 'src/css/**/styles.css';
 var otput = 'build/css'
 var autoprefixerOptions = {
   browsers: ['last 2 versions', '> 5%', 'Firefox ESR']
@@ -26,29 +30,35 @@ gulp.task('html:templates', function() {
     
 })
 gulp.task('html', function() {
-    gulp.src('src/*.html',  {since: gulp.lastRun('html')})
+    gulp.src('src/*.html')
     .pipe(debug({title: 'html rigger taking...'}))
     .pipe(rigger())
     .pipe(gulp.dest('build'))
     .pipe(debug({title: 'html rigger done...'}))
     
 })
-// CSS/SASS
-gulp.task('sass', function() {
-    return gulp.src(input)
-    .pipe(changed('build/css'))
-    .pipe(sourcemaps.init())
-    .pipe(sass().on('error', sass.logError))
+
+// CSS
+gulp.task('css', function() {
+    
+    preprocessors = [
+        postCssImport({path: ['src/css']}),
+        postCssNested,
+        postCssVars
+];
+   return gulp.src(input)
+    .pipe(postcss(preprocessors))
+    //.pipe(changed('build/css'))
     .pipe(autoprefixer(autoprefixerOptions))
     .pipe(csso({
             restructure: false,
             sourceMap: true,
             debug: true
     }))
-    .pipe(sourcemaps.write('maps'))
     .pipe(gulp.dest(otput))
     .pipe(browserSync.reload({stream: true}));
 })
+
 //IMG task
 gulp.task('img', function() {
     gulp.src(['src/img/**/*.{jpg,png,gif}', 'src/i/**/*.{jpg,png,gif}'], {base: 'src'})
@@ -60,16 +70,19 @@ gulp.task('img', function() {
     }))
     .pipe(gulp.dest('build'))
 })
+
 // SVG task
 gulp.task('svg', function(){
       gulp.src('src/i/svg/**/*.svg', {base: 'src'})
     .pipe(gulp.dest('build'))
 })
+
 //clean task
 gulp.task('clean', function() {
     gulp.src('build', {read: false})
     .pipe(clean())
 })
+
 // BS tasks
 gulp.task('browser-sync', function() {
     browserSync.init({
@@ -80,11 +93,11 @@ gulp.task('browser-sync', function() {
         browser: 'chrome'
     })
 })
-gulp.task('default', ['sass', 'watch', 'browser-sync']);
+gulp.task('default', ['css', 'watch', 'browser-sync']);
 
 // Watchers tasks
 gulp.task('watch', function() {
-    gulp.watch('src/sass/**/*.sass', ['sass'])
+    gulp.watch('src/css/**/styles.css', ['css'])
     gulp.watch('src/*.html', ['html'])
     gulp.watch('src/templates/*.html', ['html'])
     .on('change', function(event) {
